1a8f9875c1da75129548b1b633202af49692ac02
pmoreno- <pmoreno-@student.42madrid.com>
2022-08-16 17:51:35 +0200

    threads

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pmoreno- <pmoreno-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 16:00:00 by pmoreno-          #+#    #+#             */
-/*   Updated: 2022/08/16 17:13:06 by pmoreno-         ###   ########.fr       */
+/*   Updated: 2022/08/16 17:41:33 by pmoreno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,65 @@ void	init_philos_data(t_philos_data **data)
 	(*data)->philo_meals = -1;
 }
 
+t_philo	*ft_lstlast(t_philo *lst)
+{
+	t_philo	*sig;
+
+	sig = lst;
+	if (!lst)
+		return (0);
+	while (sig->right != 0)
+		sig = sig->right;
+	return (sig);
+}
+
+t_philo	*ft_lstnew(int philo)
+{
+	t_philo	*aux;
+
+	aux = malloc(sizeof(t_philo));
+	if (aux == 0)
+		return (0);
+	aux->philo = philo;
+	aux->right = NULL;
+	return (aux);
+}
+
+void	ft_lstadd_back(t_philo **philos, t_philo *new_philo)
+{
+	t_philo	*aux;
+
+	if (*philos)
+	{
+		aux = ft_lstlast(*philos);
+		aux->right = new_philo;
+	}
+	else
+		*philos = new_philo;
+}
+
+int	ft_start_philos(t_philo **philos, t_philos_data *data)
+{
+	int		i;
+	t_philo	*aux;
+
+	i = 0;
+	while (i < data->nphilos)
+	{
+		aux = ft_lstnew(i + 1);
+		if (!aux)
+		{
+			free(philos);
+			free(data);
+			return (1);
+		}
+		else
+			ft_lstadd_back(philos, aux);
+		i++;
+	}
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
 	t_philo			*philos;
@@ -35,8 +94,10 @@ int	main(int argc, char **argv)
 	atexit(leaks);
 	philos = 0;
 	data = malloc(sizeof(t_philos_data));
+	if (!data)
+		return (1);
 	init_philos_data(&data);
-	print_values(data);
+	// print_values(data);
 	if (ft_check_args(argc) == 0)
 	{
 		free(data);
@@ -51,6 +112,7 @@ int	main(int argc, char **argv)
 			return (0);
 		}
 		print_values(data);
+		ft_start_philos(&philos, data);
 	}
 	free(data);
 	return (0);
